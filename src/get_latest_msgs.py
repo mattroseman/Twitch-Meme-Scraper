79e910cceb20e5a7b6400243f2aee100c2b44381
Matthew Roseman <mrr77@case.edu>
2016-07-01 23:02:04 -0400

    finsihed writing code to print last x messages, but need to fix xpath
    Signed-off-by: Matthew Roseman <mrr77@case.edu>

--- src/get_latest_msgs.py
+++ src/get_latest_msgs.py
@@ -13,15 +13,20 @@ from lxml import html
 import requests
 
 def main():
-    if len(sys.argv) <> 2:
-        print 'There are: ', len(sys.argv), ' arguments instead of 2'
+    if len(sys.argv) <> 3:
+        print 'There are: ', len(sys.argv) - 1, ' arguments instead of 2'
         return
     url_string = sys.argv[1]
-    num_new_msgs = sys.argv[2]
+    num_new_msgs = int(sys.argv[2])
 
     page = requests.get(url_string)
     tree = html.fromstring(page.content)
 
+    messages = tree.xpath('//span[@class="message"]/text()')
+
+    for msg in messages[:num_new_msgs]:
+        print 'message: ', msg
+        
 
 if __name__ == '__main__':
     sys.exit(main())
