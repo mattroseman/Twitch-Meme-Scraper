18411c6578b1b1a481bc7d51e0657f44ebcd4b22
stohio <ig11@zips.uakron.edu>
2016-07-07 13:40:55 -0400

    more comments

--- src/main.py
+++ src/main.py
@@ -2,32 +2,32 @@ from bots import DataBot
 from threading import Thread
 import MySQLdb as mdb
 import sys
-def attach_bot(name, stream_id):
-    """thread worker function"""
-    print stream_id, name 
-    bot = DataBot.DataBot('#' + name, stream_id)
-    # bot.log_chat()
-    return
+# tryexcept block for initial mysql connection
 try:
+    # TODO move these credentials to a separate file
     con = mdb.connect('localhost', 'root', 'lolipop123', 'twitch_mining');
 
     con.autocommit(True)
     cur = con.cursor(mdb.cursors.DictCursor)
+
+    # get the list of channels to listen on
     cur.execute("SELECT * FROM streams")
     rows = cur.fetchall()
 
     for row in rows:
+        # for each channel, create a bot instance
         bot = DataBot.DataBot('#' + row['name'], row['id'])
+    # keep the main thread alive in order to keep child threads alive as well
     while True:
         pass
+
+# print mysql error messages
 except mdb.Error, e:
-  
     print "Error %d: %s" % (e.args[0],e.args[1])
     sys.exit(1)
     
+# close connection to DB
 finally:    
     if con:    
         con.close()
 
-# myBot = DataBot.DataBot("#esl_csgo", 1)
-# myBot.log_chat()
