3ffa413140d29c9eccf7210bb76c570881de40f7
stohio <ig11@zips.uakron.edu>
2016-07-07 00:44:57 -0400

    added comments

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from bots import DataBot
 
+# read from database to get list of channels to watch
+# create thread and instantiate each object to listen to the each channel
+# below is example code to instantiate
 myBot = DataBot.DataBot("beyondthesummit")
 myBot.log_chat()
