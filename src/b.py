59549db813a7db4e2d117361b32136cee7cd60bf
stohio <ig11@zips.uakron.edu>
2016-07-11 15:02:02 -0400

    added todo comments

--- src/b.py
+++ src/b.py
@@ -3,6 +3,14 @@ import requests
 import sys
 import MySQLdb as mdb
 
+# TODO figure out where exactly this goes
+# cron job run this program
+
+"""
+Main controller function that creates objects, which find the top streams for the top games
+Lots of debug code is still present
+"""
+
 try:
     # TODO move these credentials to a separate file
     con = mdb.connect('localhost', 'root', 'lolipop123', 'twitch_mining');
@@ -34,18 +42,17 @@ finally:
     if con:    
         con.close()
 
-# create object to check twitch for the top streams
-# takes parameters - how many games to check, and how many streams to check within the games
-
+# change the limit to adjust how many of the top games you want to track
 payload = {'limit' : '15'}
 b = requests.get('https://api.twitch.tv/kraken/games/top', params = payload)
 for i in range(int(payload['limit'])): 
+# create object to check twitch for the top streams
     game_name = b.json()['top'][i]['game']['name']
     print game_name
     twitch = TopStreams.TopStreams(game_name)
-# game_name = b.json()['top'][0]['game']['name']
-# twitch = TopStreams.TopStreams(game_name)
 
+# keep main thread alive
+# TODO add in thread.join() statements in order to elegantly end the program
 while True:
     pass
 
