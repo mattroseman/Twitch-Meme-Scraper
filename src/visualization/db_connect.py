03895386ea409a68768c7d49f3354de1516c6885
Matthew Roseman <mrr77@case.edu>
2016-07-14 22:10:22 -0400

    removed unused code from db_connect.py
    Signed-off-by: Matthew Roseman <mrr77@case.edu>

--- src/visualization/db_connect.py
+++ src/visualization/db_connect.py
@@ -1,29 +1,6 @@
 #!/usr/bin/python
 import sys, ConfigParser, MySQLdb
 
-config = ConfigParser.RawConfigParser()
-config.read('db.cfg')
-
-section_name = 'Database Details'
-
-try:
-    db_name = config.get(section_name, 'db_name')
-    hostname = config.get(section_name, 'hostname')
-    ip_address = config.get(section_name, 'ip_address')
-    user = config.get(section_name, 'user')
-    password = config.get(section_name, 'password')
-except ConfigParser.NoOptionError as e:
-    print ('one of the options in the config file has no value\n{0}: ' +
-           '{1}').format(e.errno, e.strerror)
-    sys.exit()
-
-db = MySQLdb.connect(host=hostname,
-                     user=user,
-                     passwd=password,
-                     db=db_name)
-
-cur = db.cursor()
-
 class SQLConnection:
     """Used to connect to a SQL database and send queries to it"""
     def __init__(self):
