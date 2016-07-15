9a38aa97d5a71b3c86d6f25626ba34eac13319a6
Steven Gates <ig11@zips.uakron.edu>
2016-07-15 16:57:16 -0400

    added thread joining implementation

--- src/b.py
+++ src/b.py
@@ -2,7 +2,7 @@ from twitch import TopStreams
 import requests
 import sys
 import MySQLdb as mdb
-
+import threading
 # TODO figure out where exactly this goes
 # cron job run this program
 
@@ -45,14 +45,16 @@ finally:
 # change the limit to adjust how many of the top games you want to track
 payload = {'limit' : '15'}
 b = requests.get('https://api.twitch.tv/kraken/games/top', params = payload)
+thread_list = list()
 for i in range(int(payload['limit'])): 
 # create object to check twitch for the top streams
     game_name = b.json()['top'][i]['game']['name']
     print game_name
-    twitch = TopStreams.TopStreams(game_name)
-
-# keep main thread alive
-# TODO add in thread.join() statements in order to elegantly end the program
-while True:
-    pass
+    thread_list.append(TopStreams.TopStreams(game_name))
+print threading.active_count(), "active threads"
+# join the threads when they are finished computing
+# main thread waits to join all other threads before terminating
+print thread_list.__sizeof__()
+for i in thread_list:
+    i.join()
 
--- src/twitch/TopStreams.py
+++ src/twitch/TopStreams.py
@@ -20,30 +20,31 @@ class TopStreams(Thread):
         r = requests.get('https://api.twitch.tv/kraken/search/streams', params = payload)
         streams_limit = int(r.json()["_total"])
         offset = 0
-        print "Stream limit: ", streams_limit
+#        print "Stream limit: ", streams_limit
         # print r.json()['streams'][0]['viewers'], r.json()['streams'][0]['channel']['name']
-        print r.json()['_links']['next']
+#        print r.json()['_links']['next']
         # sort the streams in order to find top 10 
         try: 
             while streams_limit > offset: 
                 # print streams_limit, offset
                 offset = offset + 100
-                print "----------------------------------------------------------------------------- START ITERATION ---------------------------------------------------"
+#                print "----------------------------------------------------------------------------- START ITERATION ---------------------------------------------------"
                 try: 
                     for i in range(10):
                         if r.json()['streams'][i]['game'] == self.game_name and int(r.json()['streams'][i]['viewers']) > 250:
-                            print r.json()['streams'][i]['viewers'], r.json()['streams'][i]['channel']['name'], i
+                            #print r.json()['streams'][i]['viewers'], r.json()['streams'][i]['channel']['name'], i
                             stream_list.append(str(r.json()['streams'][i]['channel']['name']))
                         else:
                             pass
                     r = requests.get(r.json()['_links']['next'])
-                except:
-                    print "list index out of range, no biggie"
-            print "FINISHED N"
-            print stream_list
+                except Exception, e:
+                    print e
+#                    print "list index out of range, no biggie"
+#            print "FINISHED N"
+#            print stream_list
             return list(set(stream_list))
         except NameError as ex:
-            print "FINISHEDEXCEPTI"
+#            print "FINISHEDEXCEPTI"
             print ex
             e = sys.exc_info()[0]
             print e
@@ -70,11 +71,11 @@ class TopStreams(Thread):
 
 
         stream_list = self.get_top()
-        print "in run function again"
-        print stream_list
+#        print "in run function again"
+#        print stream_list
         for i in stream_list:
             query = 'INSERT INTO streams (name) VALUES ("' + i + '")'
-            print query
+#            print query
             try: 
                 self.cur.execute(query)
             except mdb.Error, e:
