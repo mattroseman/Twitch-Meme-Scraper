f78400c7c2997e94f419f3bb216ac097420cf4dc
Matthew Roseman <mrr77@case.edu>
2016-09-13 23:09:21 -0400

    circles now have appropriate labeling
    Signed-off-by: Matthew Roseman <mrr77@case.edu>

--- src/visualization/channel_migration/static/migration.js
+++ src/visualization/channel_migration/static/migration.js
@@ -2,37 +2,34 @@ $(document).ready(function() {
     console.log("javascript started");
 
     var svg = d3.select("svg");
-    var circle = svg.selectAll("circle")
-        .data([32, 57, 112, 293]);
-    var circleEnter = circle.enter().append("circle");
 
-    circle.style("fill", "steelblue");
-    circle.attr("r", 30);
-
-    //circle.attr("cx", function() { return Math.random() * 720; });
-
-    circle.data([32, 57, 112]);
-    // d refers to the data bound to the circle
-    circle.attr("r", function(d) { return Math.sqrt(d); });
-    // i refers to the index of the circle in the set of circles
-    circle.attr("cx", function(d,i) { return i * 100 + 30; });
-
-    circleEnter.attr("cy", 60);
-    circleEnter.attr("cx", function(d,i) { return i * 100 + 30; });
-    circleEnter.attr("r", function(d) { return Math.sqrt(d); });
-
-    streamname = "summit1g";
+    //  Get the current list of watching users and draw it
     $.ajax({
-        url: "streams/" + streamname,
+        url: "streams",
         method: "GET",
         dataType: "json",
         success: function(result) {
-            result.forEach(function(element, index, array) {
-                if (console && console.log) {
-                    console.log("summit1g viewers ...");
-                    console.log(element.watching);
-                }
-            });
+            // configure and draw circles
+            var circles = svg.selectAll("circle")
+                             .data(result)
+                             .enter()
+                             .append("circle")
+                             .attr("class", "stream-node");
+
+            circles.attr("r", function(d) { return Math.sqrt(d.watching.length); })
+                   .attr("cx", function(d, i) { return i*200 + 30; })
+                   .attr("cy", 200)
+                   .attr("fill", "steelblue");
+
+            // configure and draw labels
+            var labels = svg.selectAll("text")
+                            .data(result)
+                            .enter()
+                            .append("text")
+                            .attr("class", "stream-label");
+            labels.text(function(d) { return d.streamname; })
+                  .attr("x", function(d, i) {return i*200 + 30; })
+                  .attr("y", 200);
         }
     });
 });
